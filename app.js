5620c26700dbb50ef307461015883e942661e890
alej-developer <alejandropm32@gmail.com>
2026-02-20 14:25:19 +0100

    Paso 5: Configuración del servidor princial y conexión a la base de datos

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const devicesRouter = require('./routes/devices')
+
+const app = express();
+const PORT = 3000; 
+
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/tienda_moviles')
+    .then(() => console.log('Conectado a MongoDB'))
+    .catch(err => console.error('Error.', err));
+
+app.use('/api/devices', devicesRouter);
+
+app.listen(PORT, () => {
+     console.log('Servidor en http://localhost:${PORT}')
+});
+
